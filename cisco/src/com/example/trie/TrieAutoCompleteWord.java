451664d413c449f5101710a5fcc043d027add72d
002DMR744 <002DMR744@WIN-QM64C0OMJ5H>
2021-09-23 13:44:53 +0530

    Trie Auto Complete

--- cisco/src/com/example/trie/TrieAutoCompleteWord.java
+++ cisco/src/com/example/trie/TrieAutoCompleteWord.java
@@ -40,20 +40,52 @@ public class TrieAutoCompleteWord {
 		}
 		currNode.wordEnd += 1;
 	}
-	
+
 	public void autoComplete(String typedWord) {
 		TrieNode currNode = root;
 		int index;
-		
-		for(int i=0;i<typedWord.length();i++) {
-			index = typedWord.charAt(i) -'a';
-			if(currNode.child[index]==null) {
+
+		for (int i = 0; i < typedWord.length(); i++) {
+			index = typedWord.charAt(i) - 'a';
+			if (currNode.child[index] == null) {
+				System.out.println("No suggestion found for " + typedWord);
+			}else {
 				
 			}
+			currNode = currNode.child[index];
 		}
+		System.out.println(typedWord);
+		preOrderTravarsal(typedWord,currNode);
+	}
+
+	private void preOrderTravarsal(String typedWord, TrieNode currNode) {
+		if(currNode==null) {
+			return;
+		}
+		
+		for(int i = 0;i<26;i++) {
+			if(currNode.child[i]==null) {
+				continue;
+			}else if(currNode.child[i].wordEnd>0){
+				System.out.println(typedWord+currNode.child[i].data);
+				preOrderTravarsal(typedWord,currNode.child[i]);
+			}else {
+				preOrderTravarsal(typedWord+currNode.child[i].data,currNode.child[i]);
+			}
+			
+		}
+		
+		
 	}
 
 	public static void main(String[] args) {
+		
+		TrieAutoCompleteWord trieAutoCompleteWord = new TrieAutoCompleteWord();
+		trieAutoCompleteWord.init();
+		trieAutoCompleteWord.insert("abc");
+		trieAutoCompleteWord.insert("abd");
+		trieAutoCompleteWord.insert("abe");
+		trieAutoCompleteWord.autoComplete("a");
 
 	}
 
